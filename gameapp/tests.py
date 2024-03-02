f419ebdb321502e3da4eddc52095e071ad8ae1cf
Neeraj Sharma <nrjsrm07@gmail.com>
2024-03-03 00:16:41 +0530

    removed unneccessary prints

--- gameapp/tests.py
+++ gameapp/tests.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 mongo_uri = "mongodb+srv://nrjsrm07:asdfasdf@brainaimcluster.80z1pzq.mongodb.net/?retryWrites=true&w=majority&appName=BrainAimCluster"
 
 client = MongoClient(mongo_uri)
-print(mongo_uri)
+
 # List the available databases
 print(client.list_database_names())
 
--- gameapp/views.py
+++ gameapp/views.py
@@ -8,15 +8,11 @@ import random
 
 def randomQuestionFromDB(level, truth):
     # Query for fetching a random question based on level and truth
-    print("inside random question from db")
     questions = Questions.objects.filter(level=level, truth_dare=truth)
     # Check if there are any questions matching the criteria
     if questions:
         # Select a random question from the queryset
-        print(questions)
         random_question = random.choice(questions)
-        print(random_question)
-        print(random_question.question)
         return random_question.question
     else:
         return "No question found for the given level and truth"
@@ -44,7 +40,6 @@ def truth_dare_view(request):
         level = request.POST['level']
         truth = request.POST['truth_dare']
         truth = truth.lower()
-        print(truth, level)
         question = randomQuestionFromDB(level, truth)
         messages.info(request, question)
         dataValues = {'level':level,'type':truth }
