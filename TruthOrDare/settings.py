352291f3f9da55288a7d971dd258b4b65fdefaa0
Neeraj Sharma <nrjsrm07@gmail.com>
2024-03-14 12:17:07 +0530

    cors changes

--- TruthOrDare/settings.py
+++ TruthOrDare/settings.py
@@ -58,9 +58,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://brain-aim.onrender.com",
+]
 ROOT_URLCONF = 'TruthOrDare.urls'
 
 TEMPLATES = [
